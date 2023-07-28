0d0fe87114dd9135177c8234b1d83fec64ba1756
Sebiten <sebdevspace@gmail.com>
2023-07-28 12:07:49 -0300

    almost

--- app/components/MyProfilePic.tsx
+++ app/components/MyProfilePic.tsx
@@ -4,7 +4,7 @@ export default function MyProfilePic() {
     return (
         <section className="w-full mx-auto">
             <Image
-                className="border-4 border-black dark:border-slate-500 drop-shadow-xl shadow-black rounded-full mx-auto mt-8"
+                className="border-4 border-black dark:border-green-300 drop-shadow-xl shadow-black rounded-full mx-auto mt-8"
                 src="/images/profilephoto.jpg"
                 width={200}
                 height={200}
