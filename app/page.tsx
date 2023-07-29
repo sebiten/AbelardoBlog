6e2ca55906f610fc5fec33d3ce846d965451ac3f
Sebiten <sebdevspace@gmail.com>
2023-07-29 12:41:03 -0300

    abel

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,39 @@
+import Script from "next/script";
 import Footer from "./components/Footer";
 import Posts from "./components/Posts";
 
 export default function Home() {
   return (
-    <main className=" mx-auto">
-      <div className="mt-12 mb-6 text-xl mx-auto max-w-2xl text-center dark:text-white lg:w-3/4 xl:w-1/2">
-        
-        <p className="mt-4">
-          Aquí tendrás acceso a una amplia variedad de artículos sobre salud moderna, tecnología, alimentación y otros temas interesantes. Mi objetivo es brindarte información útil e intrigante sobre estos temas poco comunes, pero igualmente interesantes. Así, podrás estar bien informado y conocer más sobre aspectos importantes de la vida contemporánea.
-        </p>
-        <p className="mt-4">
-          <span className="font-bold text-green-200">¡Gracias por leer!</span>
-        </p>
+    <>
+      <div className="container">
+        <Script src="https://www.googletagmanager.com/gtag/js?id=G-66SJ6716WY" />
+        <Script id="google-analytics">
+          {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+ 
+          gtag('config', 'G-66SJ6716WY');
+        `}
+        </Script>
       </div>
-      <Posts />
-      <Footer/>
-    </main>
+      <main className=" mx-auto">
+        <div className="mt-12 mb-6 text-xl mx-auto max-w-2xl text-center dark:text-white lg:w-3/4 xl:w-1/2">
+          <p className="mt-4">
+            Aquí tendrás acceso a una amplia variedad de artículos sobre salud
+            moderna, tecnología, alimentación y otros temas interesantes. Mi
+            objetivo es brindarte información útil e intrigante sobre estos
+            temas poco comunes, pero igualmente interesantes. Así, podrás estar
+            bien informado y conocer más sobre aspectos importantes de la vida
+            contemporánea.
+          </p>
+          <p className="mt-4">
+            <span className="font-bold text-green-200">¡Gracias por leer!</span>
+          </p>
+        </div>
+        <Posts />
+        <Footer />
+      </main>
+    </>
   );
 }
