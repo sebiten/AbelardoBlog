80628c258160f3c197250c2bacbed490fe03e2e7
Sebiten <sebdevspace@gmail.com>
2023-08-02 16:28:14 -0300

    asd

--- lib/posts.ts
+++ lib/posts.ts
@@ -1,58 +1,51 @@
-import fs from "fs";
-import path from "path";
-import grayMatter from "gray-matter";
-import { remark } from "remark";
-import html from "remark-html";
+const fs = require("fs");
+const path = require("path");
+const grayMatter = require("gray-matter");
+const { remark } = require("remark");
+const html = require("remark-html");
 
-export interface BlogPost {
-  id: string;
-  title: string;
-  date: string;
-  imageUrl: string;
-  categories: string[];
-}
+module.exports = {
+  getSortedPostsData() {
+    const postsDirectory = path.join(process.cwd(), "blogposts");
+    const fileNames = fs.readdirSync(postsDirectory);
+    const allPostsData = fileNames.map((fileName) => {
+      const id = fileName.replace(/\.md$/, "");
+      const fullPath = path.join(postsDirectory, fileName);
+      const fileContents = fs.readFileSync(fullPath, "utf8");
+      const matterResult = grayMatter(fileContents);
 
-const postsDirectory = path.join(process.cwd(), "blogposts");
+      const blogPost = {
+        id,
+        title: matterResult.data.title,
+        date: matterResult.data.date,
+        imageUrl: matterResult.data.imageUrl || "",
+        categories: matterResult.data.categories || [],
+      };
 
-export function getSortedPostsData() {
-  const fileNames = fs.readdirSync(postsDirectory);
-  const allPostsData = fileNames.map((fileName) => {
-    const id = fileName.replace(/\.md$/, "");
-    const fullPath = path.join(postsDirectory, fileName);
+      return blogPost;
+    });
+
+    return allPostsData.sort((a, b) => (a.date < b.date ? 1 : -1));
+  },
+
+  async getPostData(id) {
+    const postsDirectory = path.join(process.cwd(), "blogposts");
+    const fullPath = path.join(postsDirectory, `${id}.md`);
     const fileContents = fs.readFileSync(fullPath, "utf8");
     const matterResult = grayMatter(fileContents);
 
+    const processedContent = await remark().use(html).process(matterResult.content);
+    const contentHtml = processedContent.toString();
 
-    const blogPost: BlogPost = {
+    const blogPostWithHTML = {
       id,
       title: matterResult.data.title,
       date: matterResult.data.date,
+      contentHtml,
       imageUrl: matterResult.data.imageUrl || "",
       categories: matterResult.data.categories || [],
     };
 
-    return blogPost;
-  });
-
-  return allPostsData.sort((a, b) => (a.date < b.date ? 1 : -1));
-}
-
-export async function getPostData(id: string) {
-  const fullPath = path.join(postsDirectory, `${id}.md`);
-  const fileContents = fs.readFileSync(fullPath, "utf8");
-  const matterResult = grayMatter(fileContents);
-
-  const processedContent = await remark().use(html).process(matterResult.content);
-  const contentHtml = processedContent.toString();
-
-  const blogPostWithHTML: BlogPost & { contentHtml: string; imageUrl: string } = {
-    id,
-    title: matterResult.data.title,
-    date: matterResult.data.date,
-    contentHtml,
-    imageUrl: matterResult.data.imageUrl || "",
-    categories: matterResult.data.categories || [],
-  };
-
-  return blogPostWithHTML;
-}
+    return blogPostWithHTML;
+  },
+};
