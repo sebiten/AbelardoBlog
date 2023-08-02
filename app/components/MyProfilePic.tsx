d4e680e57d813eb3aa012ea3302994d72788bddf
Sebiten <sebdevspace@gmail.com>
2023-08-02 18:06:48 -0300

    blog

--- app/components/MyProfilePic.tsx
+++ app/components/MyProfilePic.tsx
@@ -8,7 +8,7 @@ export default function MyProfilePic() {
         <p className="text-center text-4xl md:text-6xl lg:text-8xl text-white font-bold uppercase animate-pulse animate-infinite animate-duration-[100ms] animate-delay-[44ms] animate-ease-out -mt-24 prose prose-xl prose-slate dark:prose-invert">
           <span className="text-yellow-500 font-normal -z-10">
             <br></br>
-            blog
+            Blog
           </span>
         </p>
       </div>
