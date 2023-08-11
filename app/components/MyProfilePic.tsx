cb143e9695a95690af8d75802663d03d93497a1f
Sebiten <sebdevspace@gmail.com>
2023-08-11 13:57:59 -0300

    cat

--- app/components/MyProfilePic.tsx
+++ app/components/MyProfilePic.tsx
@@ -4,7 +4,7 @@ export default function MyProfilePic() {
   return (
     <section className="w-full mx-auto  mt-6 overflow-hidden">
       <div className="flex flex-col justify-center items-center">
-          <Image src="/cat.png" alt="abelardo-blog" height={500} width={300} />
+          <Image src="/cat.webp" alt="abelardo-blog" height={500} width={300} />
           <h3 className="text-center text-4xl md:text-6xl lg:text-8xl text-white font-bold uppercase animate-pulse animate-infinite animate-duration-[100ms] animate-delay-[44ms] animate-ease-out -mt-24 prose prose-xl prose-slate dark:prose-invert">
             <span className="text-yellow-500 font-normal -z-10">
               <br></br>
