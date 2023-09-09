c91a5ecacc25848bdb2ea391aba4545b9416a705
Sebiten <sebdevspace@gmail.com>
2023-09-09 12:04:46 -0300

    things

--- app/components/AdSense.js
+++ app/components/AdSense.js
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 import { useEffect } from "react";
 
 const AdSense = ({ adSlot }) => {
@@ -11,8 +11,11 @@ const AdSense = ({ adSlot }) => {
   return (
     <ins
       className="adsbygoogle"
-      style={{ display: "block", margin: auto }}
-      data-ad-client="ca-pub-6692046911486022" // Replace with your publisher ID
+      style={{
+        display: "block",
+        margin: "10px auto", // 10px en top y bottom, y centrado horizontalmente
+      }}
+      data-ad-client="ca-pub-6692046911486022"
       data-ad-slot={adSlot}
       data-ad-format="auto"
       data-full-width-responsive="true"
