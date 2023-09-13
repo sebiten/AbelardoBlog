e91db0ff4525c510fbdaa33a51791ad37bcd55e3
Sebiten <sebdevspace@gmail.com>
2023-09-13 16:35:00 -0300

    no tall

--- app/layout.tsx
+++ app/layout.tsx
@@ -55,7 +55,6 @@ export default function RootLayout({
       </head>
       <body className="dark:bg-slate-800">
         <Navbar />
-        <AdSenseTallComponent />
         {children}
         <NewsletterSubscribe />
         <Footer />
