4e3b6e86046d4a735485239b6d6954f8d934b48f
Sebiten <sebdevspace@gmail.com>
2023-10-28 17:44:31 -0300

    asd

--- app/components/Apis/Clima.js
+++ app/components/Apis/Clima.js
@@ -60,7 +60,7 @@ const Clima = () => {
               </p>
             </div>
             <form className="mx-auto relative">
-              <div className="max-w-[120px] max-h-[50px] bg-gray-300 bottom-2 absolute">
+              <div className="max-w-[120px] max-h-[50px] bottom-2 absolute">
                 <AdUnit
                   className="adsbygoogle inline-block w-[120px] h-[50px]"
                   data-ad-client="ca-pub-6692046911486022"
